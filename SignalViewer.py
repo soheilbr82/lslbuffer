bdbe59dfbec069cdc09ff095e6bb4b4861ea73dd
Josh Dunkley <jdunkley@vols.utk.edu>
2019-12-02 15:18:03 -0500

    adding signal viewer

--- SignalViewer.py
+++ SignalViewer.py
@@ -6,6 +6,7 @@ import pylsl
 from scipy.signal import butter, lfilter, savgol_coeffs
 from PyQt5 import QtCore, QtGui, QtWidgets
 from scipy import signal, stats
+from queue import Queue
 #import sys
 #from pynfb.signal_processing.filters import NotchFilter, IdentityFilter, FilterSequence
 
@@ -39,6 +40,7 @@ class SignalViewer(pg.PlotWidget):
         self.n_samples = int(fs * seconds_to_plot) # samples to show
         self.x_stamps = np.arange(self.n_samples)
         self.previous_pos = 0 # resieved samples counter
+        self.pos = 0
         self.x_mesh = np.linspace(0, seconds_to_plot, self.n_samples)
         self.y_raw_buffer = np.zeros(shape=(self.n_samples, self.n_signals)) * np.nan
 
@@ -67,35 +69,41 @@ class SignalViewer(pg.PlotWidget):
         else:
             self.notch_filter = None
 
-    def update(self, chunk):
+    def update(self, chunk, setX=None, setPos=None):
         # estimate current pos
         #chunk = self.inlet.pull_chunk(timeout=0.0)
         chunk_len = len(chunk)
         current_pos = (self.previous_pos + chunk_len) % self.n_samples
-        current_x = self.x_mesh[current_pos]
+        if setX and setPos and self.pos != 0:
+            self.current_x = setX
+            self.current_pos = setPos
+            self.pos=0
+        else:
+            self.current_x = self.x_mesh[current_pos]
+            self.current_pos = (self.previous_pos + chunk_len) % self.n_samples
 
         # notch filter
         if self.notch_filter is not None and self.notch_filter_check_box.isChecked():
             chunk = self.notch_filter.apply(chunk)
 
         # update buffer
-        if self.previous_pos < current_pos:
-            self.y_raw_buffer[self.previous_pos:current_pos] = chunk[:, np.array(self.indices)]
+        if self.previous_pos < self.current_pos:
+            self.y_raw_buffer[self.previous_pos:self.current_pos] = chunk[:, np.array(self.indices)]
         else:
             self.y_raw_buffer[self.previous_pos:] = chunk[:self.n_samples - self.previous_pos, np.array(self.indices)]
-            if current_pos > 0:
-                self.y_raw_buffer[:current_pos] = chunk[self.n_samples - self.previous_pos:, np.array(self.indices)]
+            if self.current_pos > 0:
+                self.y_raw_buffer[:self.current_pos] = chunk[self.n_samples - self.previous_pos:, np.array(self.indices)]
 
         # pre-process y data and update it
         y_data = self.prepare_y_data(chunk_len)
-        before_mask = (self.x_stamps < current_pos)
+        before_mask = (self.x_stamps < self.current_pos)
         for i, curve in enumerate(self.curves):
             y = y_data[:, i] if i < y_data.shape[1] else self.x_mesh * np.nan
             curve.setData(self.x_mesh, y, connect=np.isfinite(y) | before_mask)
-        self.vertical_line.setValue(current_x)
+        self.vertical_line.setValue(self.current_x)
 
         # update pos
-        self.previous_pos = current_pos
+        self.previous_pos = self.current_pos
 
     def prepare_y_data(self, chunk_len):
         return self.y_raw_buffer
@@ -254,15 +262,27 @@ class runSignal:
         self.label1 = label1
         self.notch_filter=None
         self.butter_filter=None
+        self.apply_filters = False
+        self.q = Queue()
+        
         
     
-    def setViewer(self, layout1, filterlayout):
+    def setViewer(self, layout1, filters=None, band=None):
         self.w = RawSignalViewer(self.fs, self.n_channels, self.view_channels)
         self.layout1=layout1
         self.layout1.addWidget(self.w)
         self.w.show()
 
-        self.filterlayout = filterlayout
+        if filters and band:
+            print("Band and Filters is not None")
+            self.filters = filters
+            self.band = band
+            self.band["low"].setPlaceholderText("Minimum: 0.1")
+            self.band["high"].setPlaceholderText("Maximum: %.3f" % float(self.fs/2-.001))
+            self.low = None
+            self.high = None
+            self.w2 = RawSignalViewer(self.fs, self.n_channels, self.view_channels)
+            self.w3 = RawSignalViewer(self.fs, self.n_channels, self.view_channels)
         
         self.timer = QtCore.QElapsedTimer()
         self.time = 0
@@ -307,12 +327,21 @@ class runSignal:
             print("timer is inactive")
     
     def updateData(self):
+        time_elapsed = (self.timer.elapsed()/1000)
+
+        #Create a buffer of the last four seconds of data acquired
+        #Time buffer can be adjusted based on the amount of seconds of data needed
+        if time_elapsed <= 4:
+            self.q.put(self.chunk)
+        else:
+            self.q.get()
+            self.q.put(self.chunk)
   
         #Update display based on seconds, minutes, or hours ran
-        if (self.timer.elapsed()/1000) < 60: #If less than a minute has gone by
+        if time_elapsed < 60: #If less than a minute has gone by
             self.label1.setText(" Stream: %s\t\t Sampling rate: %d\t\t Time: %.2fs\t\t Chunk size: %d" \
                 %(self.lsl.stream_name, self.lsl.get_frequency(), self.timer.elapsed()/1000, len(self.chunk)))
-        elif (self.timer.elapsed()/1000) < 3600:#If less than an hour has gone by
+        elif time_elapsed < 3600:#If less than an hour has gone by
             self.label1.setText(" Stream: %s\t\t Sampling rate: %d\t\t Time: %dm %.2fs\t\t Chunk size: %d" \
                 %(self.lsl.stream_name, self.lsl.get_frequency(), int((self.timer.elapsed()/1000)/60), \
                     (self.timer.elapsed()/1000)%60, len(self.chunk)))
@@ -321,26 +350,87 @@ class runSignal:
                 %(self.lsl.stream_name, self.lsl.get_frequency(), int(self.timer.elapsed()/3600), \
                     int((self.timer.elapsed()/1000)/60)%60 , (self.timer.elapsed()/1000)%60, len(self.chunk)))
 
+    #Defines low band pass
+    def lowPass(self):
+        if len(self.band["low"].text()) == 0:
+            self.low = None
+        else:
+            if 0.1 <= float(self.band["low"].text()):
+                self.low = float(self.band["low"].text())
+                print(self.low)
+
+    #Defines high band pass
+    def highPass(self):
+        if len(self.band["high"].text()) == 0:
+            self.high = None
+        else:
+            if float(self.band["high"].text()) < self.fs/2:
+                self.high = float(self.band["high"].text())
+            
+
+    #Applies filters to real-time EEG data
+    #Windows for each filter will appear when button is clicked
+    #Window will not disappear unless another filter or no filter is specified and 
     def applyFilter(self):
-        if self.filterlayout.itemAt(0).widget().isChecked():
-            pass 
-        elif self.filterlayout.itemAt(1).widget().isChecked():
-            self.butter_filter=None
-            self.notch_filter = NotchFilter(50, self.fs, len(self.n_channels))
-            self.chunk = self.notch_filter.apply(self.chunk)
-        elif self.filterlayout.itemAt(2).widget().isChecked():
-            self.notch_filter=None
-            self.butter_filter = ButterFilter((9, 100), self.fs, len(self.n_channels))
-            self.chunk = self.butter_filter.apply(self.chunk)
+
+        if self.filters["noFilter"].isChecked():
+            print("No filter")
+            if self.w2.isVisible():
+                    self.w2.close()
+            if self.w3.isVisible():
+                    self.w3.close()
+
+            if self.apply_filters == True:
+                self.apply_filters = False
+
+        else:
+            if self.filters["notch"].isChecked():
+                if self.apply_filters == False:
+                    self.apply_filters = True
+
+                if self.w2.isVisible():
+                    pass
+                else:
+                    print("Notch")
+                    self.w2.show()
+                    self.w2.setWindowTitle("Notch Filter")
+
+                self.notch_filter = NotchFilter(60, self.fs, len(self.n_channels))
+                self.w2.update(self.notch_filter.apply(self.chunk))
+            if self.filters["butter"].isChecked():
+                if self.apply_filters == False:
+                    self.apply_filters = True
+
+                if self.w3.isVisible():
+                    pass
+                else:
+                    print("Butter")
+                    self.w3.show()
+                    self.w3.setWindowTitle("Butter Filter")
+
+                self.band["low"].editingFinished.connect(self.lowPass)
+                self.band["high"].editingFinished.connect(self.highPass)
+    
+                if self.low or self.high:
+                    self.butter_filter = ButterFilter((self.low, self.high), self.fs, len(self.n_channels))
+                elif self.low == None and self.high == None:
+                    self.butter_filter = ButterFilter((0.1, (self.fs/2)-0.001), self.fs, len(self.n_channels))
+
+                self.w3.update(self.butter_filter.apply(self.chunk))
+
+            
+
 
     def update(self):
-        self.time
         self.time += self.chunk_len
         self.chunk, timestamp = self.lsl.get_next_chunk()
-        self.applyFilter()
-
+        
         if self.chunk is not None: #Update signal and signal data if pulled chunk is not None
             self.w.update(self.chunk)
+            if self.apply_filters == True:
+                self.applyFilter()
+            else:
+                pass
             self.updateData()
   
 
