aa5f1be96c1a7fd2a5e3511d3e030da9f2f89a67
Josh Dunkley <jdunkley@vols.utk.edu>
2019-12-13 15:16:29 -0500

    Updating VerseStream.py

--- VersaStream.py
+++ VersaStream.py
@@ -1,5 +1,22 @@
-from LSL_visualization import LSLgui
+from sys import platform
+from subprocess import Popen, PIPE, STDOUT, run
 
 if __name__ == '__main__':
-    app = LSLgui() #Instantiate instance of the LSL gui object
-    app.start() #Start the app
+    if platform == "linux" or platform == "linux2":
+        from LSL_visualization import LSLgui
+
+        # Instantiate instance of the LSL gui object
+        app = LSLgui()
+        # Start the app
+        app.start()
+    elif platform == "darwin":
+        from LSL_visualization import LSLgui
+
+        # Instantiate instance of the LSL gui object
+        app = LSLgui()
+        # Start the app
+        app.start()
+    elif platform == "win32":
+        p = Popen(['ipython', '--pylab=qt'], stdout=PIPE,  stdin=PIPE, stderr=STDOUT)
+        grep_stdout = p.communicate(input=b'from LSL_visualization import LSLgui\nimport LSLgui\napp = LSLgui('
+                                          b')\napp.start()\n')[0]
